bfcd942e8d3b4add6aa917fecdb3837a960092c6
josuesitop <143621811+josuesitop@users.noreply.github.com>
2023-09-15 08:23:55 -0600

    Add files via upload

---
+++ Areatriaungulo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriaungulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Areatriaungulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Declarar variables
+        double base, altura, area;
+        
+        
+        // Calcular el area del triangulo
+        base = 10;
+        altura = 18;
+        
+        area = (base * altura) / 2;
+        //Mostrar el resultado
+        System.out.println("El area del triangulo es:" + area);
+        
+    }
+    
+}
---
+++ Listadetareas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listadetareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Listadetareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "hacer practicas en java" ;
+        String tarea2 = "limpiar el arenero de mi gato" ;
+        String tarea3 = "bañarme" ;
+        String tarea4 = "hacerme de cenar" ;
+        String tarea5 = "hacerme skincare" ;
+        
+        System.out.println("soy josue y estas son las cosas que hago diario;"+ tarea1 + tarea2 + tarea3 + tarea4 + tarea5);
+        
+    }
+    
+}
